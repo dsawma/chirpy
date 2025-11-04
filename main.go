5e741e4d6fcd946ff2a9d32f6f37af605fe351ed
dsawma <davidthanga13@gmail.com>
2025-11-03 22:34:25 -0500

    B)start of routing

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
+func (cfg *apiConfig) middlewareMetricsInc(handler http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+}
+func (cfg *apiConfig) numOfRequest(handler http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+}
+
+func NewConfig(i int32) *apiConfig {
+	c := &apiConfig{}
+	c.fileserverHits.Store(i)
+	return c
+}
+
+func main() {
+	apiC := NewConfig(0)
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	mux.Handle("/app/", apiC.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 	mux.HandleFunc("/healthz", handlerReadiness)
 
+	hits := http.NewServeMux()
+	hits.Handle("")
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
@@ -25,3 +45,5 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
+
+
